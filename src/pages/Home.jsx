846a06fd7b336d54a87278838309b52bf54e987f
Ma1si <islamhick@gmail.com>
2026-04-28 20:50:29 +0300

    site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ticher from '../assets/ticher.jpg'
-import './styles/Home.css'
 function Home() {
   return (
     <div className="home">
@@ -9,12 +8,12 @@ function Home() {
           <img src={ticher} className="base" alt="teacher" />
 
           <div className="info">
-            <h1 className="name">
+            <h1 className="name_ticher">
               Нигматуллина Лилия Ильдаровна
             </h1>
             <p className="txt">
               Дорогие ребята и уважаемые родители, лето - это время отдыха и открытий.
-              Этот сайт поможет вам сделать каникулы безопасными, интересными и продуктивными
+              Этот сайт поможет вам сделать каникулы безопасными, интересными и продуктивными.
             </p>
           </div>
         </div>
